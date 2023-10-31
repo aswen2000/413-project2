1e31d2f90751192d8508dd506dfa16eef44001d9
aswen2000 <aswen2000@gmail.com>
2023-10-31 15:32:10 -0500

    correctly modifying diff file

--- apply_changes.py
+++ apply_changes.py
@@ -1,14 +1,20 @@
+import reset_files
+
+reset_diff = reset_files
+
 def apply_changes(chunks):
+    reset_diff.reset_diff_output()
+
     chunk_counter = -1
+    lines = []
 
     with open('diff_output.patch', 'r') as file:
         lines = file.readlines()
 
-        for index, line in enumerate(lines):
-            print(line)
+    with open('diff_output.patch', 'w') as file:
+        for line in lines:
 
             if line.startswith('@@'):
                 chunk_counter = chunk_counter + 1
-            if not chunk_counter < 0 and not chunk_counter in chunks:
-                lines[index] = ""
-    print(lines)
+            if chunk_counter < 0 or chunk_counter in chunks:
+                file.write(line)
--- reset_files.py
+++ reset_files.py
@@ -5,7 +5,10 @@ def reset_file1v1():
     destination_file = 'temp_copy.java'
     shutil.copy(source_file, destination_file)
 
-
+def reset_diff_output():
+    source_file = 'perm_diff.patch'
+    destination_file = 'diff_output.patch'
+    shutil.copy(source_file, destination_file)
 
 
 
