3055eb5e301561f9d8d218eb7e3a611e045de958
aswen2000 <aswen2000@gmail.com>
2023-10-31 15:07:16 -0500

    change temp copy, not og

--- index.py
+++ index.py
@@ -9,3 +9,4 @@ app_change.reset_file1v1()
 diff_creator.create_diff()
 
 shutil.copy('diff_output.patch', 'perm_diff.patch') #creates permanent copy of diff file so don't have to regen every time it is changed
+
--- reset_files.py
+++ reset_files.py
@@ -1,8 +1,8 @@
 import shutil
 
 def reset_file1v1():
-    source_file = 'perm_copy.java'
-    destination_file = 'file1v1.java'
+    source_file = 'file1v1.java'
+    destination_file = 'temp_copy.java'
     shutil.copy(source_file, destination_file)
 
 
