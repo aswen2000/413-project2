2dc3f95e9adbd88e3f8dbf756f90893fdb65a1a6
aswen2000 <aswen2000@gmail.com>
2023-10-31 14:59:55 -0500

    creating temp & perm diff files

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 import reset_files
-import create_diff
+import diff
+import shutil
 
 app_change = reset_files
-diff_creator = create_diff
+diff_creator = diff
 
 app_change.reset_file1v1()
 diff_creator.create_diff()
+
+shutil.copy('diff_output.patch', 'perm_diff.patch') #creates permanent copy of diff file so don't have to regen every time it is changed
