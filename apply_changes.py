d039d8e8d80e207e1c36887638ccb6e54a36e06d
aswen2000 <aswen2000@gmail.com>
2023-10-31 15:22:47 -0500

    correctly parsing out needed changes

--- apply_changes.py
+++ apply_changes.py
@@ -1,2 +1,14 @@
 def apply_changes(chunks):
-    
+    chunk_counter = -1
+
+    with open('diff_output.patch', 'r') as file:
+        lines = file.readlines()
+
+        for index, line in enumerate(lines):
+            print(line)
+
+            if line.startswith('@@'):
+                chunk_counter = chunk_counter + 1
+            if not chunk_counter < 0 and not chunk_counter in chunks:
+                lines[index] = ""
+    print(lines)
--- index.py
+++ index.py
@@ -1,12 +1,15 @@
 import reset_files
 import diff
+import apply_changes
 import shutil
 
 app_change = reset_files
 diff_creator = diff
+change_applier = apply_changes
 
 app_change.reset_file1v1()
 diff_creator.create_diff()
 
 shutil.copy('diff_output.patch', 'perm_diff.patch') #creates permanent copy of diff file so don't have to regen every time it is changed
 
+change_applier.apply_changes([0,1,2,3])
